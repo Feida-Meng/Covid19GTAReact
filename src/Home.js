89d35f5afb69b4947ab5bb2a023e2bfc0723eb08
Feida <feidameng01@gmail.com>
2020-04-28 20:42:21 -0400

    create index html and root component

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+
+	render() {
+
+		return (
+			<div
+				style={{ height: '100%', width: '100%' }}
+				id={'main'}
+			>
+
+
+			</div>
+		);
+	}
+
+}
+
+
+
+
