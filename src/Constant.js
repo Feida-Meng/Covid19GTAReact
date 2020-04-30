da388dbda935edfcd8fcdf2dafa95153933dbd8b
Feida <feidameng01@gmail.com>
2020-04-29 21:02:18 -0400

    create function to get all cases by city and date

--- src/Constant.js
+++ src/Constant.js
@@ -3,3 +3,11 @@ export const GOOGLE_API_KEY = 'AIzaSyDWe3ODZ4v8CDWs44xjHJXAmD-TqauwGhY';
 
 export const baseUrl = process.env.NODE_ENV === 'production' ? 'https://api.covid19gta.com/api/v1' : 'http://localhost:8000/api/v1';
 
+export const cityColorCode = [
+	'#E57373','#263238','#EC407A','#BDBDBD','#8E24AA',
+	'#2196F3','#4527A0','#FF5722','#FFEB3B','#CDDC39',
+	'#4DB6AC','#1B5E20','#C5E1A5','#B71C1C','#BDC3C7',
+	'#626567','#7D6608','#512E5F','#D35400','#73C6B6',
+	'#33CC00','#3366CC','#FFFF33','#CC0066','#CCFFFF'
+
+];
