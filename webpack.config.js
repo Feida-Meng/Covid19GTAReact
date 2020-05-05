9c08bb729839b0910e3489eb2c0c4a0978a510a3
Feida <feidameng01@gmail.com>
2020-05-05 08:42:36 -0400

    Refactored webpack.config.js to use the env variable

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,57 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-	devtool: "cheap-module-source-map",
-	entry: {
-		app: './src/index.js'
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: "bundle.js"
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html'
-		}),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_module/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.css$/i,
-				loader: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader'
-				],
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader',
-				],
-			},
-		]
+module.exports = env => {
+
+	return {
+		devtool: "cheap-module-source-map",
+		entry: {
+			app: './src/index.js'
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: "bundle.js"
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './public/index.html'
+			}),
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': env.NODE_ENV || 'development'
+			})
+		],
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_module/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.css$/i,
+					loader: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.(png|svg|jpg|gif)$/,
+					use: [
+						'file-loader'
+					],
+				},
+				{
+					test: /\.(woff|woff2|eot|ttf|otf)$/,
+					use: [
+						'file-loader',
+					],
+				},
+			]
 
 
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		port: 9000
+		},
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			compress: true,
+			port: 9000
+		}
+
 	}
-
 };
